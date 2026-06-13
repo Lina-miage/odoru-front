a83cae4451e5a1fb7ac01725e3e9cdf8a378f3bd
Lina-miage <lina.mohamadi@toulouse.miage.fr>
2026-06-13 18:54:24 +0200

    Affichage du tableau des membres et création du form d'inscription d'un membre

--- src/app/features/membres/membres.component.ts
+++ src/app/features/membres/membres.component.ts
@@ -1,25 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { UtilisateurService } from '../../services/utilisateur.service';
 import { Utilisateur } from '../../model/utilisateur.model';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
-import { TagModule } from 'primeng/tag';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { CardModule } from 'primeng/card';
+import { FloatLabel } from 'primeng/floatlabel';
+import { PasswordModule } from 'primeng/password';
+
 
 @Component({
   selector: 'app-membres',
   standalone: true,
-  imports: [CommonModule, TableModule, ButtonModule, TagModule],
+  imports: [CommonModule, FormsModule, TableModule, ButtonModule, InputTextModule, InputNumberModule, CardModule, FloatLabel, PasswordModule],
   templateUrl: './membres.component.html'
 })
 export class MembreComponent implements OnInit {
   membres: Utilisateur[] = [];
+  showForm = false;
+  
+  nouveauMembre: Utilisateur = {
+    nom: '',
+    prenom: '',
+    courriel: '',
+    nomUtilisateur: '',
+    motDePasse: '',
+    niveauExpertise: 1
+  };
 
   constructor(private utilisateurService: UtilisateurService) {}
 
   ngOnInit() {
+    this.chargerMembres();
+  }
+
+  chargerMembres() {
     this.utilisateurService.getAll().subscribe(data => {
       this.membres = data;
+    });
+  }
+
+  inscrire() {
+    this.utilisateurService.inscrire(this.nouveauMembre).subscribe(() => {
+      this.chargerMembres();
+      this.nouveauMembre = {
+        nom: '',
+        prenom: '',
+        courriel: '',
+        nomUtilisateur: '',
+        motDePasse: '',
+        niveauExpertise: 1
+      };
     });
   }
 }
